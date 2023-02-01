cb083e581e2925985a9f7d76df1163beb2590e54
Nik Jakimovec <imkolias@gmail.com>
2023-02-01 14:08:04 +0300

    some fixes, migration fix, settings debug fix, empty file remove, models fix, load_data some fix, logging added

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import logging
 from dataclasses import astuple, asdict
 from contextlib import contextmanager
 
@@ -50,11 +51,9 @@ class SQLiteExtractor:
     def __init__(self, connection: sqlite3.Connection, pgcon: _connection):
         self.pgcurs = pgcon.cursor()
         self.curs = connection.cursor()
-        print('[Class init complete]')
 
     def data_insert(self, mdata: list, datatable: str):
         """PUT data in Postgres DB by parts"""
-        print('[W]', end="")
         sql_params = []
         for elem in mdata:
 
@@ -73,36 +72,41 @@ class SQLiteExtractor:
     def extract_all_and_push(self):
         """Go through tables list in config DATATABLES_LIST,
         select SLICESIZE from DB, and push DATA_LIST to DATA_INSERT method"""
-        print("[Start copy process]")
-
+        # print("[Start copy process]")
+        logging.info("Start copy process")
         for tablename, configvalue in datatables_list.items():
             row_count = 0
-            print(f'[SELECT TABLE] "{tablename}"')
-            sql_query = f"SELECT * FROM {tablename};"
-            self.curs.execute(sql_query)
-            print("    ", end="")
+            try:
+                sql_query = f"SELECT * FROM {tablename};"
+                self.curs.execute(sql_query)
+            except:
+                logging.error(f'Table "{tablename}" not found, but should be')
             class_name = globals()[configvalue]
 
             # Process SQL return as BATCH
             while True:
-                print('[R]', end="")
-                data_list = []
+                rowsto_insert = []
                 result = self.curs.fetchmany(slicesize)
                 if not result:
-                    print(f' ({row_count} rows writed)')
+                    if row_count > 0:
+                        logging.info(f'Table: "{tablename}" - '
+                                     f'{row_count} row successfully written')
+                    else:
+                        logging.warning(f'Table: "{tablename}" - '
+                                        f'ZERO(0) rows written')
                     break
 
-                # Here code get ALL SQL BATCH return (SLICESIZE = 500rows), and
-                # put it in DATA_LIST list
+                # Here code get ALL SQL BATCH return (SLICESIZE = 500rows),
+                # and put it in 'rowsto_insert' list
                 for row in result:
-                    data_list += [class_name(**data_reformat(dict(row)))]
+                    rowsto_insert += [class_name(**data_reformat(dict(row)))]
 
-                row_count += len(data_list)
+                row_count += len(rowsto_insert)
 
-                # Take DATA_LIST (500 rows by default) and send it to
+                # Take 'rowsto_insert' (500 rows by default) and send it to
                 # POSTGRES DB, using data_insert funct
                 # so code use changeable memory count for store sql return
-                self.data_insert(data_list, tablename)
+                self.data_insert(rowsto_insert, tablename)
 
 
 def movedata(connection: sqlite3.Connection, pg_conn: _connection):
@@ -115,7 +119,7 @@ def movedata(connection: sqlite3.Connection, pg_conn: _connection):
                            insert all extracted data to
                            Postrges DB
     """
-    print("[All connections - ok]")
+    logging.info("All connections - OK")
     sqlite_extractor = SQLiteExtractor(connection, pg_conn)
 
     sqlite_extractor.extract_all_and_push()
@@ -124,14 +128,25 @@ def movedata(connection: sqlite3.Connection, pg_conn: _connection):
 if __name__ == '__main__':
     sqlitedbfile = 'db.sqlite'
 
-    if os.path.exists(sqlitedbfile):
+    logging.basicConfig(
+        level=logging.INFO, filename="load_data.log", filemode="w",
+                        format="%(asctime)s %(levelname)s %(message)s")
 
+    if os.path.exists(sqlitedbfile):
+        logging.info(f'Source SQLite file found: "{sqlitedbfile}"')
         try:
             with conn_context(sqlitedbfile) as sqlite_conn, \
                     psycopg2.connect(**dsl, cursor_factory=DictCursor) as pgc:
                 movedata(sqlite_conn, pgc)
         except psycopg2.OperationalError:
-            print('[Error] - Can\'t connect to Postgres DB')
-
+            logging.critical('Can\'t connect to target Postgres DB '
+                             '(OperationalError)')
+        finally:
+            if sqlite_conn:
+                sqlite_conn.close()
+                logging.info('SQLite connection closed')
+            if pgc:
+                pgc.close()
+                logging.info('Postgres connection closed')
     else:
-        print('[Error] - Can\'t find source Sqlite3 DB file:', sqlitedbfile)
+        logging.critical(f"Can't find source Sqlite3 DB file: {sqlitedbfile}")
